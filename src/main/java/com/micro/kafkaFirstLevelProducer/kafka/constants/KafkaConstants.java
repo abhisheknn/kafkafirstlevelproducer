435cc504abeb3e6eac7ee7bbb5c564509a0001ab
Nagpure <abhisheknagpure08@gmail.com>
2019-03-10 23:37:21 +0530

    add: dockerx prefix to topic

--- src/main/java/com/micro/kafkaFirstLevelProducer/kafka/constants/KafkaConstants.java
+++ src/main/java/com/micro/kafkaFirstLevelProducer/kafka/constants/KafkaConstants.java
@@ -7,24 +7,24 @@ import java.util.Map;
 public class KafkaConstants {
 	public static final String KAFKA_BROKERS = System.getenv("KAFKABROKERS");
 	public static final String CLIENT_ID = "client1";
-	public static final  String CONTAINERINFO_TOPIC_NAME = "container_details";
-	public static final String DELETEDCONTAINERLIST_TOPIC_NAME = "deleted_containerid";
+	public static final  String CONTAINERINFO_TOPIC_NAME = "dockerx.container_details";
+	public static final String DELETEDCONTAINERLIST_TOPIC_NAME = "dockerx.deleted_containerid";
 	public static final String OFFSET_RESET_LATEST = "latest";
 	public static final String OFFSET_RESET_EARLIER = "earliest";
 	public static final Integer MAX_POLL_RECORDS = 1;
 	public static final String JWTOKEN = "jwtoken";
-	public static final String FILE_DIFF_TOPIC_NAME = "file_diff";
-	public static final String COMMANDS_TOPIC_NAME = "commands";
-	public static final String PROCESSES_TOPIC_NAME = "processes";
-	public static final String CONTAINERLIST_TOPIC_NAME = "container_list";
-	public static final String NETWORK_LIST_TOPIC_NAME = "network_list";
-	public static final String NETWORK_TOPIC_NAME = "network_details";
-	public static final String NETWORK_DELETED_TOPIC_NAME = "deleted_networkid";
+	public static final String FILE_DIFF_TOPIC_NAME = "dockerx.file_diff";
+	public static final String COMMANDS_TOPIC_NAME = "dockerx.commands";
+	public static final String PROCESSES_TOPIC_NAME = "dockerx.processes";
+	public static final String CONTAINERLIST_TOPIC_NAME = "dockerx.container_list";
+	public static final String NETWORK_LIST_TOPIC_NAME = "dockerx.network_list";
+	public static final String NETWORK_TOPIC_NAME = "dockerx.network_details";
+	public static final String NETWORK_DELETED_TOPIC_NAME = "dockerx.deleted_networkid";
 	
 	public static final Map<String, String> TOPICS= initMap();
-	private static final String VOLUME_TOPIC_NAME = "volume_details";
-	private static final String VOLUME_DELETED_TOPIC_NAME = "deleted_volumeid";
-	private static final String VOLUMELIST_TOPIC_NAME = "volume_list";
+	private static final String VOLUME_TOPIC_NAME = "dockerx.volume_details";
+	private static final String VOLUME_DELETED_TOPIC_NAME = "dockerx.deleted_volumeid";
+	private static final String VOLUMELIST_TOPIC_NAME = "dockerx.volume_list";
 	
 	private static Map<String, String> initMap() {
 	Map<String, String> map= new HashMap<>();
